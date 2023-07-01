3103a6fb25e335db1e6d68d03acd169c716b3c67
dcalenzani <dcalenzani@pucp.pe>
2023-07-01 00:58:38 -0500

    Second Commit, big updates on functioning, correct CSV output and LibreTranslate integration

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DND Translator (v.1)
-curl -u 'dcalenzani' https://api.github.com/user/repos -d '{"name":"AcaCrawler","description":"Crawler for academic databases"}'
-I made this translator for a dnd spell book, but I guess it works for any kind of CSV, and with some refactoring maybe any table. 
+I made this translator for a dnd spell book, but I guess it works for any kind of CSV, and with some refactoring maybe any table. Currently I'm using LibreTranslate, which has a couple of requirements and was kind of hard for me to run, notice that I have had many troubles with AI installations before, some of its dependencies are quite sensible so make sure you are using a virtual environment. 
 
-Its a simple program, you input a csv file, you get a csv file translated. Right now you must pre-select the language.
+The choice of LibreTranslate was made after the first commit, when troubles around the uses of third-party APIs was clear for a tabulation traduction of large data sets.
+
+Its a simple program, you input a csv file, you get a csv file translated. Right now you must pre-select the language in the code, so make sure you are selecting the correct language and that you have that language installed within LibreTranslate.
--- translator.py
+++ translator.py
@@ -1,7 +1,17 @@
 import csv
-from translate import Translator
+import requests
 
 file_path = input('Ingrese la ruta del CSV a traducir:\n')
+
+# Specify the URL of your LibreTranslate server
+server_url = 'http://localhost:5000'
+
+# Translation parameters
+from_lang = 'en'
+to_lang = 'es'
+
+# API endpoint for translation
+api_endpoint = f'{server_url}/translate'
 
 # Read the CSV file
 with open(file_path, 'r') as file:
@@ -9,19 +19,36 @@ with open(file_path, 'r') as file:
     csv_data = list(reader)
 
 # Perform translation
-translator = Translator(to_lang="es")
 translated_data = []
 total_lines = len(csv_data)
 completed_lines = 0
 for row in csv_data:
+    text = row
+    payload = {
+    'q': text,
+    'source': from_lang,
+    'target': to_lang
+    }
 
     # Print completion advancement
-    translated_row = [translator.translate(cell) for cell in row]
-    translated_data.append(translated_row)
-        
     completed_lines += 1
     progress = (completed_lines / total_lines) * 100
     print(f"Progress: {completed_lines}/{total_lines} ({progress:.2f}%)")
+
+    try:
+        # Send POST request to the server
+        response = requests.post(api_endpoint, json=payload)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            translated_text = response.json()['translatedText']
+            print(f'Translated text: {translated_text}')
+            translated_data.append([translated_text])  # Append translated text to the list
+        else:
+            print(f'Error: {response.status_code} - {response.text}')
+
+    except requests.exceptions.RequestException as e:
+        print(f'Request Exception: {e}')
 
 # Write translated data to a new CSV file
 with open('translation.csv', 'w', newline='') as file:
